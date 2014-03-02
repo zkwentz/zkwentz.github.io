23e39c00b1f6ca446d0e4fdf0d1e8b35083f796f
Zach Wentz <zkwentz@gmail.com>
2014-03-02 00:23:59 -0800

    portfolio site beta

--- js/main.js
+++ js/main.js
@@ -1,8 +1,37 @@
 require(["jquery"], function($) {
-    //This function is called when scripts/helper/util.js is loaded.
-    //If util.js calls define(), then this function is not fired until
-    //util's dependencies have loaded, and the util argument will hold
-    //the module value for "helper/util".
+
+    $(document).ready(function() {
+
+        // set your twitter id
+        var user = 'wentz__';
+
+        $.ajax({
+            url: 'https://api.twitter.com/1/statuses/user_timeline.json',
+            contentType: "application/json",
+            dataType: 'jsonp',
+            data: {
+                'screen_name':'wentz__',
+                'count':1
+            },
+            success: function(data) {
+                // result returned
+                var tweet = data[0].text;
+
+                // process links and reply
+                tweet = tweet.replace(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig, function(url) {
+                    return ''+url+'';
+                }).replace(/B@([_a-z0-9]+)/ig, function(reply) {
+                    return  reply.charAt(0)+''+reply.substring(1)+'';
+                });
+
+                // output the result
+                $(".latest-tweet").html(tweet);
+            },
+            error: function(e) {
+               console.log(e.message);
+            }
+        });
+
+    });
     
-    console.log($);
 });
